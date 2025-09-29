9a03ad5f6e7eb4056c69dc05b57386ac7e016533
leesunghyun1019 <166623569+leesunghyun1019@users.noreply.github.com>
2025-09-29 10:45:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,34 @@ from train import train
 
 def main():
     # 하이퍼파라미터 설정
-    BATCH_SIZE = 128
-    EPOCHS = 50
-    LEARNING_RATE = 0.01 
+    BATCH_SIZE = 256
+    EPOCHS = 512
+    LEARNING_RATE = 0.0001
     NUM_CLASSES = 10
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"torch.cuda.is_available :{torch.cuda.is_available()}")
 
     # 데이터 로더 준비
     trainloader, testloader = get_cifar10_loaders(
         batch_size=BATCH_SIZE, 
-        num_workers=4
+        num_workers=8
     )
 
     # 모델 초기화
     model = Reactnet(
         num_classes=NUM_CLASSES, 
         imagenet=False,  # CIFAR-10이므로 False
-        input_sign=True
+        input_sign=False
     )
+
+    model=model.to(device)
 
     # 모델 학습
     model = train(
         model, 
         trainloader, 
-        testloader, 
+        testloader,
+        device=device, 
         epochs=EPOCHS,
         lr=LEARNING_RATE
     )
